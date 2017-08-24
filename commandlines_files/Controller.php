fd793f66f1d5608d527dd022419df5647a35efc3
ignition123 <sudeep.ignition@gmail.com>
2017-08-25 02:23:23 +0530

    Add files via upload

--- commandlines_files/Controller.php
+++ commandlines_files/Controller.php
@@ -1,42 +1,12 @@
 <?php
 	
-	use Kernel\App\Core\Orm;
+	use Kernel\App\Core\Response;
 
 	class Controller
 	{
 		public function __construct($input)
 		{
-			$OrmObj = new Orm();
-
-			$bindkey = [
-				2
-			];
-
-			$OrmObj->select("*")->from("users")->where("id = ?");
-
-			if($OrmObj->:rowCount() == 1)
-			{
-				$data = [
-					'status'=>true,
-					'msg'=>'message is in json',
-					'data'=>$input
-				];
-
-				$headers = [
-					'Content-Type'=>'application/json',
-					'Author'=>'Sudeep Dasgupta'
-				];
-
-				unset($OrmObj);
-
-				response::end('json',$data,$headers);
-
-				views::render('index',"View is working even in controller");
-			}
-			else
-			{
-				response::end('json',['status'=>false,'msg'=>'No data found']);
-			}
+			response::end('json',['status'=>true,'msg'=>'Controller working']);
 		}
 	}
 ?>
